758577d0373c98fe210ac70d9eccf4da280efb1a
Jason Tseng <jason.f.tseng@gmail.com>
2017-05-21 21:32:37 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
+
 def reformat_languages(languages)
+  new_hash = {}
   # your code here
+  new_hash = languages[:oo].merge(languages[:functional])
+  new_hash.each { |language, attribute| new_hash[language][:style] = Array.new }
+  new_hash.each do |language, attribute|
+    if languages[:oo].keys.include?(language)
+      new_hash[language][:style] << :oo
+    end
+  end
+  new_hash.each do |language, attribute|
+    if languages[:functional].keys.include?(language)
+      new_hash[language][:style] << :functional
+    end
+  end
+  new_hash
 end
